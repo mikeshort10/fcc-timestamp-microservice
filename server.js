4fc710b1980b2bf64c9b1042f48d8c9d76834577
Mike Short <mikeshort2014@gmail.com>
2020-04-14 19:59:31 -0400

    updated server logic

--- server.js
+++ server.js
@@ -1,58 +1,37 @@
-// server.js
-// where your node app starts
+const express = require("express");
+const cors = require("cors");
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-app.get("/api/timestamp/:time", function (req, res) {
-  var input = req.params.time;
-  if(isNaN(new Date(Number(input)))===true&&isNaN(new Date(input))) {
-    res.json({error:"Invalid Date"})
-  } else {
-  if (input==Number(input)) {
-    input = Number(input)
-    console.log(input)
+const getDate = (time) => {
+  if (time == null) {
+    return new Date();
   }
-  var date = new Date(input)
-  res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    })
-  }
-  
-});
+  const date = Number.isInteger(+time) ? new Date(+time) : new Date(time);
+  return date == "Invalid Date" ||
+    date.toISOString().slice(0, 10) === time ||
+    date.valueOf() === +time
+    ? date
+    : new Date("Invalid Date");
+};
 
-app.get(/api\/timestamp\/?/, function(req,res) {
-    var date = new Date();
+const app = express()
+  .use(cors({ optionSuccessStatus: 200 })) // some legacy browsers choke on 204
+  .use(express.static("public")) // http://expressjs.com/en/starter/static-files.html
+  .get("/api/timestamp/:time", (req, res) => {
+    const date = getDate(req.params.time);
+    const isInvalid = date == "Invalid Date";
     res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    })
-})
-
-app.get("/", function(req,res) {
-  res.sendFile(__dirname + '/views/index.html')
-})
-
-// http://expressjs.com/en/starter/basic-routing.html
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
+      unix: isInvalid ? null : date.getTime(),
+      utc: isInvalid ? date.toString() : date.toUTCString(),
+    });
+  })
+  .get("/api/timestamp", (_, res) => {
+    const date = new Date();
+    return res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+  })
+  .get("/", (_, res) => res.sendFile(__dirname + "/views/index.html"));
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+  console.log(`http://localhost:${listener.address().port}`);
 });
